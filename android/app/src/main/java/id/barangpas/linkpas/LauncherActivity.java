ab3275a44969f780e643d6a2bf57bb3fa77d115b
AFNAN NUZULA SUGIHARTONO <afnannuzulasugihartono@gmail.com>
2026-09-15 08:15:07 +0700

    feat(android): initialize native diagnostics in launcher

--- android/app/src/main/java/id/barangpas/linkpas/LauncherActivity.java
+++ android/app/src/main/java/id/barangpas/linkpas/LauncherActivity.java
@@ -1,5 +1,15 @@
 package id.barangpas.linkpas;
 
+import android.os.Bundle;
+
 public class LauncherActivity
         extends com.google.androidbrowserhelper.trusted.LauncherActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        NativeDiagnostics.install(this, getIntent());
+        NativeDiagnostics.markStage(this, "launcher_on_create");
+        super.onCreate(savedInstanceState);
+        NativeDiagnostics.markStage(this, "twa_launch_requested");
+    }
 }
