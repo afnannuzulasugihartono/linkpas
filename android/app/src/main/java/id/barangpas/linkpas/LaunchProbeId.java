251a6c7d9e8ed725a29bed59def3c1b42e562fba
AFNAN NUZULA SUGIHARTONO <afnannuzulasugihartono@gmail.com>
2026-09-15 16:15:45 +0700

    refactor(android): make probe preservation rule testable

--- android/app/src/main/java/id/barangpas/linkpas/LaunchProbeId.java
+++ android/app/src/main/java/id/barangpas/linkpas/LaunchProbeId.java
@@ -26,4 +26,9 @@ final class LaunchProbeId {
     static String current() {
         return current;
     }
+
+    static String preferExisting(String existingProbe, String currentProbe) {
+        if (isValid(existingProbe)) return existingProbe;
+        return isValid(currentProbe) ? currentProbe : null;
+    }
 }
